56ff5f17542e6b0289096264034c4ce6276da5b0
gjmh-unikent <g.haddow@kent.ac.uk>
2015-09-01 11:41:13 +0100

    display popular posts on homepage

--- index.php
+++ index.php
@@ -1,5 +1,18 @@
 <?php get_template_part('templates/page', 'header');
 
+$popular = kentblogs_popular_get_posts();
+
+?>
+<div class="posts well popular">
+    <h2 class="h1">Trending posts</h2>
+    <div class="posts">
+        <?php foreach($popular as $post){
+            include('templates/grid-post.php');
+        } ?>
+    </div>
+</div>
+
+<?php
 $aggregate_data = get_site_option('wp-multisite-post-aggregate');
 
 if($aggregate_data === false){
@@ -11,11 +24,14 @@ if(isset($_REQUEST['page']) && is_numeric($_REQUEST['page']) && intval($_REQUEST
 }
 $aggregate_data = array_slice($aggregate_data,(($p-1)*6),6);
 ?>
-<?php foreach($aggregate_data as $post){
-    include('templates/grid-post.php');
-} ?>
-<div id="latest-foot" class="row">
-    <div class="col-sm-6 col-sm-offset-3">
-        <button class="btn btn-kent btn-lg btn-block more-results" id="more-button" type="button" data-page="1">Show more <i class="kf-spinner kf-spin" id="more-spinner" style="display: none;"></i></button>
+<h2 class="h1" >Latest Posts</h2>
+<div class="posts latest">
+    <?php foreach($aggregate_data as $post){
+        include('templates/grid-post.php');
+    } ?>
+    <div id="latest-foot" class="row">
+        <div class="col-sm-6 col-sm-offset-3">
+            <button class="btn btn-kent btn-lg btn-block more-results" id="more-button" type="button" data-page="1">Show more <i class="kf-spinner kf-spin" id="more-spinner" style="display: none;"></i></button>
+        </div>
     </div>
 </div>
