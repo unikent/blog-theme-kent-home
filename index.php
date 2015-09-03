eb9c0960a46d9bc1525954c02ce3295f00e523ce
unknown <jna@PC7MISC141.ad.kent.ac.uk>
2015-09-03 10:11:30 +0100

    simplify

--- index.php
+++ index.php
@@ -10,42 +10,36 @@ if ($popular_posts === false) {
 <div class="well posts popular">
 	<h2 class="h1">Trending posts</h2>
 	<div class="row">
-		
 		<?php foreach ($popular_posts as $post) {
 			include('templates/grid-post.php');
-		}
-		?>
+		} ?>
 	</div>
 </div>
 
 <?php
 
+// get and display aggregate data
 $aggregate_data = get_site_option('wp-multisite-post-aggregate');
+
+if($aggregate_data === false){
+	$aggregate_data = kentblogs_aggregator_init_posts();
+}
+
+$p=1;
+if(isset($_REQUEST['page']) && is_numeric($_REQUEST['page']) && intval($_REQUEST['page'])<=10){
+	$p = intval($_REQUEST['page']);
+}
+
+$aggregate_data = array_slice($aggregate_data,(($p-1)*6),6);
 
 ?>
 
 <div class="posts latest">
 	<h2 class="h1" >Latest Posts</h2>
 	<div class="row">
-		
-		<?php
-		// get and display aggregate data
-		$aggregate_data = get_site_option('wp-multisite-post-aggregate');
-
-		if($aggregate_data === false){
-			$aggregate_data = kentblogs_aggregator_init_posts();
-		}
-		$p=1;
-		if(isset($_REQUEST['page']) && is_numeric($_REQUEST['page']) && intval($_REQUEST['page'])<=10){
-			$p = intval($_REQUEST['page']);
-		}
-		$aggregate_data = array_slice($aggregate_data,(($p-1)*6),6);
-
-		foreach($aggregate_data as $post){
+		<?php foreach($aggregate_data as $post) {
 			include('templates/grid-post.php');
-		}
-
-		?>
+		} ?>
 	</div>
 </div>
 
